9c9da30758329360f4ce01304b97e2b52a383a3a
sprucegroose <1verkruyse@406-imac21.tahomasd.private>
2015-05-08 09:09:03 -0700

    Major overhaul
    Added maps with ints as keys and ArrayLists of string as their values,
    added straightFlush and royalFlush methods

--- Hand.java
+++ Hand.java
@@ -33,8 +33,64 @@ public class Hand implements Comparable {
    High Card
    WORST
    */
-   public String handValue() {
+   public String handValue()
+   {
+     Collections.sort(hand);
+     Map<String, ArrayList<Integer>> happyending = new HashMap<String, ArrayList<Integer>>();
+     for(Card ref: hand)
+     {
+        if(!happyending.containsKey(ref.suit))
+        {
+          ArrayList<Integer> original = new ArrayList<Integer>();
+          original.add(ref.value);
+          happyending.put(ref.suit, original);
+        }
+        else
+        {
+          ArrayList<Integer> appender = happyending.get(ref.suit);
+          appender.add(ref.value);
+          happyending.put(ref.suit, appender);
+        }
+     }
+     if(royalFlush(happyending))
+        System.out.println(hand);
+     if(straightFlush(happyending))
+        System.out.println(hand);
+
      return "TODO: String of Best Hand; may need helper methods";
+   }
+
+   public boolean royalFlush(Map<String, ArrayList<Integer>> happyending)
+   {
+     if(happyending.size() == 1)
+     {
+       ArrayList<Integer> start = happyending.get(hand.get(0).suit);
+       int starter = start.get(0);
+       if(starter != 10)
+        return false;
+       for(int i = 1; i < hand.size(); i++)
+       {
+         if(start.get(0) != starter + 1)
+          return false;
+       }
+       return true;
+     }
+     return false;
+   }
+   public boolean straightFlush(Map<String, ArrayList<Integer>> happyending)
+   {
+     if(happyending.size() == 1)
+     {
+       ArrayList<Integer> start = happyending.get(hand.get(0).suit);
+       int starter = start.get(0);
+       for(int i = 1; i < hand.size(); i++)
+       {
+         if(start.get(0) != starter + 1)
+          return false;
+       }
+       return true;
+     }
+     return false;
    }
 
    public int compareTo(Object x){
